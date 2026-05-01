75d408e394356a95866b105299610068f29bb883
Abrar <muhammadabrar341@gmail.com>
2026-05-01 17:11:00 +0500

    fixed route issue

--- proxy.ts
+++ proxy.ts
@@ -9,10 +9,12 @@ import {
 
 export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
+  
   const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+  req,
+  secret: process.env.NEXTAUTH_SECRET,
+  secureCookie: process.env.NODE_ENV === "production",
+});
 
   const isLoggedIn = Boolean(token);
 
